52ab89dae2202cb55ad43835a6b9a1a6e998b66c
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-22 23:30:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,19 @@
 # Make your shoe class here!
-class Shoe
+# class Shoe
   
-  def initialize(brand)
-    @brand = brand
-  end
-  attr_reader :brand
-  attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+#   attr_accessor :color, :size, :material, :condition
+  
+#   def initialize(brand)
+#     @brand = brand
+#   end
+  
  
 
  
- def cobble
-   self.condition = "new"
-   puts "Your shoe is as good as new!" 
- end
+# def cobble
+#   self.condition = "new"
+#   puts "Your shoe is as good as new!" 
+# end
   
-end
+# end
