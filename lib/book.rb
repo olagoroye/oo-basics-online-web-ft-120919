6cd7241af144805db74d133aa613c8dd5030ca3f
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-22 23:29:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,11 @@
 class Book
+  attr_accessor :title
   def initialize(title)
     @title = title
   end
-  def title
-    @title
-  end
+  # def title
+  #   @title
+  # end
   def author=(author)
     @author = author
   end
